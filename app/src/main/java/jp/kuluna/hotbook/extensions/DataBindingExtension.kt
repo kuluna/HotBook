6fb5b1d08dfa031a8abe57d0e7bdd78e9ff2d4d4
kuluna <mailkuluna@gmail.com>
2018-04-10 21:46:11 +0900

    update libraries

--- app/src/main/java/jp/kuluna/hotbook/extensions/DataBindingExtension.kt
+++ app/src/main/java/jp/kuluna/hotbook/extensions/DataBindingExtension.kt
@@ -3,12 +3,10 @@ package jp.kuluna.hotbook.extensions
 import android.databinding.BindingAdapter
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
-import jp.kuluna.hotbook.R
-import jp.kuluna.hotbook.models.EntryImage
 
-@BindingAdapter("app:loadImage")
-fun loadImageAsync(imageView: ImageView, url: String?) {
+@BindingAdapter("loadImage")
+fun ImageView.loadImageAsync(url: String?) {
     url?.let {
-        Picasso.with(imageView.context).load(it).into(imageView)
+        Picasso.get().load(it).into(this)
     }
 }
